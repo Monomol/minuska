800c3542d5e1f8f15a5fe4bc74e171c9f6b7891a
Monomol <misa.louda@gmail.com>
2025-05-10 22:08:52 +0000

    feat: implemented proof of concept tests

---
+++ coq-minuska/theories/martelli_montanari_tests.v
@@ -0,0 +1,60 @@
+From Minuska Require Import
+    prelude
+    spec
+    martelli_montanari
+    basic_properties
+    empty
+    default_everything
+    trivial
+.
+
+Instance sm : StaticModel := @DSM mysignature β MyProgramInfo.
+
+Definition dec_paper_input1 : list (TermOver BuiltinOrVar) := [
+    t_term "f"
+        [
+            t_over (bov_variable "x1");
+            t_term "g"
+                [t_term "a" [];
+                    t_term "f"
+                        [ t_over (bov_variable "x5"); t_term "b" [] ]
+                ]
+        ];
+    t_term "f"
+        [
+            t_term "h"
+                [ t_term "c" [] ];
+            t_term "g"
+                [t_over (bov_variable "x2");
+                    t_term "f"
+                        [ t_term "b" []; t_over (bov_variable "x5") ]
+                ]
+        ];
+    t_term "f"
+        [
+            t_term "h"
+                [ t_over (bov_variable "x4") ];
+            t_term "g"
+                [t_over (bov_variable "x6");
+                 t_over (bov_variable "x3")
+                ]
+        ]
+    ]
+.
+
+Compute (@dec sm dec_paper_input1).
+
+Definition unify_paper1_input1 : TermOver BuiltinOrVar := (t_term "f" [
+  t_over (bov_variable "x1");
+  t_term "g" [t_over (bov_variable "x2"); t_over (bov_variable "x3")];
+  t_over (bov_variable "x2");
+  t_term "b" []]).
+Definition unify_paper1_input2 : TermOver BuiltinOrVar := (t_term "f" [
+  t_term "g" [t_term "h" [t_term "a" []; t_over (bov_variable "x5")]; t_over (bov_variable "x2")];
+  t_over (bov_variable "x1");
+  t_term "h" [t_term "a" []; t_over (bov_variable "x4")];
+  t_over (bov_variable "x4")]).
+
+Compute (@init_r sm U_listset_ops [unify_paper1_input1; unify_paper1_input2]).
+
+Compute (@unify_terms sm U_listset_ops [unify_paper1_input1; unify_paper1_input2]).
